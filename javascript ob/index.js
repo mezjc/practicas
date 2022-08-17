9841047c6349ffcbee7b08f86ec8a59f60dde7fc
mezjc <110258170+mezjc@users.noreply.github.com>
2022-08-17 14:51:50 -0500

    Add files via upload

---
+++ javascript ob/index.js
@@ -0,0 +1,12 @@
+let lista = [
+    "Jhonnatan",
+    30,
+    true,
+    new Date(1992, 4, 4),
+    {
+    titulo: "Viaje al centro de la tierra",
+    autor: "Julio Verne",
+    fecha: new Date(2002, 1, 1),
+    url: "https://www.amazon.com/Viaje-Al-Centro-Tierra/dp/8447326322",
+    },
+];    
